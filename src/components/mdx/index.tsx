a9688729fee1e3b1084e27f872c9da69f7265749
6-keem <6ukeem@gmail.com>
2025-01-04 20:16:53 +0900

    refactor: update styles

--- src/components/mdx/index.tsx
+++ src/components/mdx/index.tsx
@@ -2,10 +2,12 @@ import { Callout } from "./Callout";
 import { Image } from "./Image";
 import { ExternalLink } from "./Link";
 import { MDXComponents } from "mdx/types";
+import { Thumbnail } from "./Thumbnail";
 
 export const MDXComponent: MDXComponents = {
     a: ExternalLink as any,
     img: Image as any,
     blockquote: Callout,
     Callout,
+    Thumbnail: Thumbnail
 };
