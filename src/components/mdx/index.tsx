bad3c7169a66c8bd095d9a87083316238398a5a8
6-keem <6ukeem@gmail.com>
2025-02-09 20:20:18 +0900

    feat: customize code block to apply copy button

--- src/components/mdx/index.tsx
+++ src/components/mdx/index.tsx
@@ -1,15 +1,17 @@
-import { Callout } from "./Callout";
-import { Image } from "./Image";
-import { ExternalLink } from "./Link";
-import { MDXComponents } from "mdx/types";
-import { Thumbnail } from "./Thumbnail";
-import { Highlight } from "./Highlight";
+import { Callout } from './Callout';
+import { Image } from './Image';
+import { ExternalLink } from './Link';
+import { MDXComponents } from 'mdx/types';
+import { Thumbnail } from './Thumbnail';
+import { Highlight } from './Highlight';
+import CodeBlock from './CodeBlock';
 
 export const MDXComponent: MDXComponents = {
-    a: ExternalLink as any,
-    img: Image as any,
-    blockquote: Callout,
-    Callout,
-    Thumbnail: Thumbnail,
-    Highlight: Highlight
+  a: ExternalLink as any,
+  img: Image as any,
+  blockquote: Callout,
+  Callout,
+  Thumbnail: Thumbnail,
+  Highlight: Highlight,
+  pre: (props: any) => <CodeBlock {...props} />,
 };
