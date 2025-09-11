d4b492c796ceb44a6d4330f582ef76d0867c19ba
6-keem <6ukeem@gmail.com>
2025-09-11 12:56:46 +0900

    feat: add query parameter

--- src/app/api/badge/route.ts
+++ src/app/api/badge/route.ts
@@ -1,17 +1,18 @@
-import { NextRequest } from "next/server";
-import BlogBadge from "./Badge";
+import { NextRequest } from 'next/server';
+import BlogBadge from './Badge';
 
 export async function GET(request: NextRequest) {
-    const { searchParams } = new URL(request.url);
-    const width = searchParams.get("width") ?? "450";
-    const height = searchParams.get("height") ?? "130";
-    const date = searchParams.get("date") ?? null;
+  const { searchParams } = new URL(request.url);
+  const width = searchParams.get('width') ?? '450';
+  const height = searchParams.get('height') ?? '130';
+  const category = searchParams.get('category') ?? null;
+  const date = searchParams.get('date') ?? null;
 
-    const badge = await BlogBadge({ width, height, date });
-    return new Response(badge, {
-        status: 200,
-        headers: {
-            "Content-Type": "image/svg+xml",
-        },
-    });
+  const badge = await BlogBadge({ width, height, date, category });
+  return new Response(badge, {
+    status: 200,
+    headers: {
+      'Content-Type': 'image/svg+xml',
+    },
+  });
 }
