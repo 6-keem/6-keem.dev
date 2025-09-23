5bc897cee32a65f74a593b63f14613d4dde323ae
6-keem <6ukeem@gmail.com>
2025-09-23 19:23:59 +0900

    docs: change word

--- src/data/resume-data-jp.tsx
+++ src/data/resume-data-jp.tsx
@@ -4,11 +4,11 @@ export const RESUME_DATA_JP: ResumeData = {
   initials: 'サンジュン',
   location: 'ソウル、韓国、KST',
   locationLink: 'https://www.google.com/maps/place/seoul',
-  about: 'フルスタック開発者',
+  about: 'フルスタックエンジニア',
   summary: `
-**行動力**と**諦めない粘り強さ**で問題を解決し、成長する開発者です。
+**行動力**と**諦めない粘り強さ**で問題を解決し、成長するエンジニアです。
 新しい技術への**好奇心**と**継続的な学習習慣**を基に、能力を絶えず拡張し、**ユーザー中心の思考**でより良い体験を提供するサービスを作りたいと考えています。
-また、**チームメンバーとの協力**を通じて個人の成長を超え、**良い成果**を共に生み出すことを重視しており、それを基に着実に成長する**フルスタック開発者**になることを目指しています。
+また、**チームメンバーとの協力**を通じて個人の成長を超え、**良い成果**を共に生み出すことを重視しており、それを基に着実に成長する**フルスタックエンジニア**になることを目指しています。
 `,
 
   avatarUrl: 'https://avatars.githubusercontent.com/u/113224939?v=4',
@@ -40,7 +40,7 @@ export const RESUME_DATA_JP: ResumeData = {
     {
       company: 'Every I',
       link: '',
-      title: 'フルスタック開発者',
+      title: 'フルスタックエンジニア',
       start: '2025.06',
       end: undefined,
       description: '漢城大学 産学連携プロジェクト',
